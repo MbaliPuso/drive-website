c472d33a0f5b8ebb7b4dfbb531e7e79eb8cb84d7
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-06-27 00:58:25 +0200

    import update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import AppRoutes from "./AppRoutes";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
